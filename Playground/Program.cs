34a4c2671f6fd0dc3d62a4de2ebd4bec2d18436c
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-09 00:13:33 +0200

    Added Playground.

---
+++ Playground/Program.cs
@@ -0,0 +1,20 @@
+﻿using Models;
+using System;
+
+namespace Playground
+{
+    class Program
+    {
+        static void Main()
+        {
+            var from = new Address("123");
+            var to = new Address("456");
+            var amount = 10.0m;
+            var sig = "123";
+            uint blockId = 0;
+            var tx = new Transaction(from, to, amount, sig, blockId);
+
+            Console.WriteLine(tx.Hash);
+        }
+    }
+}
