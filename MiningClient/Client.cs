bba6cb9951c8a12107814b7aa8a8a427ba9618ac
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-09 19:58:17 +0200

    Removed unnecessary usings.

--- MiningClient/Client.cs
+++ MiningClient/Client.cs
@@ -1,9 +1,7 @@
 ﻿using Models;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 
 namespace MiningClient
