745c164b6311c0d165d6cee55ef9ef8330934c61
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 11:45:37 +0200

    Added MessageSignerVerifier

---
+++ Services/Cryptography/IMessageSignerVerifier.cs
@@ -0,0 +1,11 @@
+﻿using Secp256k1;
+
+namespace Services.Cryptography
+{
+    public interface IMessageSignerVerifier
+    {
+        SignedMessage GetMessageSignature(string privateKey, string message);
+
+        bool MessageVerified(SignedMessage message);
+    }
+}
---
+++ Services/Cryptography/MessageSignerVerifier.cs
@@ -0,0 +1,17 @@
+﻿using Secp256k1;
+
+namespace Services.Cryptography
+{
+    public class MessageSignerVerifier : Secp256k1.MessageSignerVerifier, IMessageSignerVerifier
+    {
+        public SignedMessage GetMessageSignature(string privateKey, string message)
+        {
+            return base.Sign(Hex.HexToBigInteger(privateKey), message);
+        }
+
+        public bool MessageVerified(SignedMessage message)
+        {
+            return base.Verify(message);
+        }
+    }
+}
