50cf4e5c2019dffb233ec6327f1a750c6e5f6455
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 11:46:00 +0200

    Added TransactionSecurityService.

---
+++ Services/Cryptography/ITransactionSecurityService.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+using Secp256k1;
+
+namespace Services.Cryptography
+{
+    public interface ITransactionSecurityService
+    {
+        SignedMessage GetTransactionSignature(Transaction transaction, string privateKey);
+
+        bool TransactionVerified(SignedMessage signedTransaction);
+    }
+}
---
+++ Services/Cryptography/TransactionSecurityService.cs
@@ -0,0 +1,26 @@
+﻿using Models;
+using Secp256k1;
+
+namespace Services.Cryptography
+{
+    public class TransactionSecurityService : ITransactionSecurityService
+    {
+        private IMessageSignerVerifier messageSignerVerifier;
+
+        public TransactionSecurityService(IMessageSignerVerifier messageSignerVerifier)
+        {
+            this.messageSignerVerifier = messageSignerVerifier;
+        }
+
+        public SignedMessage GetTransactionSignature(Transaction transaction, string privateKey)
+        {
+            return this.messageSignerVerifier.GetMessageSignature(
+                privateKey, transaction.GetMetadataString());
+        }
+
+        public bool TransactionVerified(SignedMessage signedTransaction)
+        {
+            return this.messageSignerVerifier.MessageVerified(signedTransaction);
+        }
+    }
+}
