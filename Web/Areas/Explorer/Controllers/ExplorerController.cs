f399b2f23797cf635373fdb2f61bde068e8733f3
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 19:33:27 +0200

    Added Explorer Area.

---
+++ Web/Areas/Explorer/Controllers/ExplorerController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Nodes;
+
+namespace Web.Controllers
+{
+    [Area("Explorer")]
+    [Route("[area]")]
+    [Route("[area]/[controller]")]
+    public class ExplorerController : Controller
+    {
+        private INodeService nodeService;
+
+        public ExplorerController(INodeService nodeService)
+        {
+            this.nodeService = nodeService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("[action]")]
+        [Route("[area]/[action]")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
