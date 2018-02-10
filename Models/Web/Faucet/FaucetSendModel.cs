b48310b1c804103037ea2c4c83b29738261364ba
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 16:52:59 +0200

    Added PrivateKey and From.

--- Models/Web/Faucet/FaucetSendModel.cs
+++ Models/Web/Faucet/FaucetSendModel.cs
@@ -2,8 +2,12 @@
 {
     public class FaucetSendModel
     {
+        public string From { get; set; }
+
         public string To { get; set; }
 
         public decimal Amount { get; set; }
+
+        public string PrivateKey { get; set; }
     }
 }
