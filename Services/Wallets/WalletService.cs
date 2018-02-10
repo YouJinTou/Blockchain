eb097018415923a2ca5525dddf365d93c205b26c
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 08:01:57 +0200

    Added INodeService as a dependency.

--- Services/Wallets/WalletService.cs
+++ Services/Wallets/WalletService.cs
@@ -1,9 +1,17 @@
 ﻿using Models;
+using Services.Nodes;
 
 namespace Services.Wallets
 {
     public class WalletService : IWalletService
     {
+        private INodeService nodeService;
+
+        public WalletService(INodeService nodeService)
+        {
+            this.nodeService = nodeService;
+        }
+
         public WalletCredentials CreateWallet(string password)
         {
             var wallet = new Wallet(password);
@@ -12,6 +20,13 @@ namespace Services.Wallets
                 Address = wallet.Address,
                 PrivateKey = wallet.PrivateKey
             };
+
+            this.nodeService.RegisterAddress(
+                new Models.Web.Users.RegisterUserModel
+                {
+                    Address = credentials.Address,
+                    Balance = 0.0m
+                });
 
             return credentials;
         }
