cdf50947899f6b588eaf89db871e011562ad917a
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 19:37:35 +0200

    Added ExplorerViewModel

---
+++ Web/Areas/Explorer/Models/ExplorerViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Web.Areas.Explorer.Models
+{
+    public class ExplorerViewModel
+    {
+        public int Id { get; set; }
+
+        public int TransactionsCount { get; set; }
+
+        public int Difficulty { get; set; }
+
+        public string Hash { get; set; }
+
+        public string PreviousHash { get; set; }
+
+        public string MinedBy { get; set; }
+
+        public ulong Nonce { get; set; }
+
+        public DateTime MinedOn { get; set; }
+    }
+}
