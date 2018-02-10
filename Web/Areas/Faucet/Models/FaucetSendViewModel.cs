4b0a7559b0445090277cbfa3c6a810c00fa95cd2
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 17:17:14 +0200

    Completed the Index view.

--- Web/Areas/Faucet/Models/FaucetSendViewModel.cs
+++ Web/Areas/Faucet/Models/FaucetSendViewModel.cs
@@ -1,11 +1,16 @@
-﻿namespace Web.Areas.Faucet.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Areas.Faucet.Models
 {
     public class FaucetSendViewModel
     {
         public decimal Balance { get; set; }
 
+        [Required]
         public string To { get; set; }
 
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Invalid amount.")]
         public decimal Amount { get; set; }
     }
 }
