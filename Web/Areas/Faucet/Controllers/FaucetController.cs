0befd44d2c2556bf37a3c18ecdf79157c2596423
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 16:50:44 +0200

    Implemented FaucetService.

--- Web/Areas/Faucet/Controllers/FaucetController.cs
+++ Web/Areas/Faucet/Controllers/FaucetController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Models.Web.Faucet;
+using Models.Web.Wallets;
+using Services.Faucet;
+using System;
+using Web.Areas.Faucet.Models;
 
 namespace Web.Areas.Faucet.Controllers
 {
@@ -7,12 +13,40 @@ namespace Web.Areas.Faucet.Controllers
     [Route("[area]/[controller]")]
     public class FaucetController : Controller
     {
+        private IFaucetService faucetService;
+
+        public FaucetController(IFaucetService faucetService)
+        {
+            this.faucetService = faucetService;
+        }
+
         [Route("")]
         [Route("[action]")]
         [Route("[area]/[action]")]
-        public IActionResult Index()
+        [HttpGet]
+        public IActionResult Index(string message)
         {
-            return View();
+            ViewBag.Message = message;
+
+            return View(new FaucetSendViewModel());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Route("[action]")]
+        public IActionResult Send([FromForm]FaucetSendViewModel model)
+        {
+            try
+            {
+                this.faucetService.SendFunds(
+                    Mapper.Map<FaucetSendViewModel, FaucetSendModel>(model));
+
+                return View("Create", model);
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Create", new { ex.Message });
+            }
         }
     }
 }
