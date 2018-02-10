048fcbb3f91ee0cc0b4a151b401b6d5139598758
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-10 19:44:56 +0200

    Added ExplorerViewModel display template.

--- Web/Areas/Explorer/Models/ExplorerViewModel.cs
+++ Web/Areas/Explorer/Models/ExplorerViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Web.Areas.Explorer.Models
 {
@@ -6,18 +7,22 @@ namespace Web.Areas.Explorer.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Transactions Count")]
         public int TransactionsCount { get; set; }
 
         public int Difficulty { get; set; }
 
         public string Hash { get; set; }
 
+        [Display(Name = "Last Hash")]
         public string PreviousHash { get; set; }
 
+        [Display(Name = "Miner")]
         public string MinedBy { get; set; }
 
         public ulong Nonce { get; set; }
 
+        [Display(Name = "Timestamp")]
         public DateTime MinedOn { get; set; }
     }
 }
