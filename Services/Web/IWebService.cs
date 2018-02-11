ed50951b5d9ff51689fd518cc4341ff13fe80b3f
Dimitar Andreev Dimitrov <dandreevd@gmail.com>
2018-02-11 12:02:37 +0200

    Addd WebService.

---
+++ Services/Web/IWebService.cs
@@ -0,0 +1,9 @@
+﻿namespace Services.Web
+{
+    public interface IWebService
+    {
+        T GetDeserialized<T>(string endpoint) where T : class;
+
+        string SendSerialized<T>(string endpoint, T obj) where T : class;
+    }
+}
---
+++ Services/Web/WebService.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
+
+namespace Services.Web
+{
+    public class WebService : IWebService
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public T GetDeserialized<T>(string endpoint) where T : class
+        {
+            var responseString = client.GetStringAsync(endpoint).Result;
+            var obj = JsonConvert.DeserializeObject<T>(responseString);
+
+            return obj;
+        }
+
+        public string SendSerialized<T>(string endpoint, T obj) where T : class
+        {
+            var content = new StringContent(
+                JsonConvert.SerializeObject(T), Encoding.UTF8, "application/json");
+            var response = client.PostAsync(new Uri(endpoint), content).Result;
+            var responseString = response.Content.ReadAsStringAsync().Result;
+
+            return responseString;
+        }
+    }
+}
